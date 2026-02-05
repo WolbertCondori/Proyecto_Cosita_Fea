1c1bdf8cc655424295fb454e79b12072cffcc1c8
wolbert <wolbertcondoribendezu@gmail.com>
2026-02-05 22:38:38 +0100

    v1.2

--- TiendaCompraYvende/src/app/layouts/main-layout/components/head-main/head-main.ts
+++ TiendaCompraYvende/src/app/layouts/main-layout/components/head-main/head-main.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
+import {LoginMain} from '../login-main/login-main';
 
 @Component({
   selector: 'app-head-main',
   imports: [
-    RouterLink
+    RouterLink,
+    LoginMain
   ],
   templateUrl: './head-main.html',
   styleUrl: './head-main.scss',
 })
 export class HeadMain {
+  openLogin = signal<boolean>(false)
   constructor(private router: Router) {}
 
+  openedLogin(): void {
+    this.openLogin.update(state=>!state);
+  }
 
 }
--- TiendaCompraYvende/src/app/layouts/main-layout/components/login-main/login-main.ts
+++ TiendaCompraYvende/src/app/layouts/main-layout/components/login-main/login-main.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, output, signal} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-main',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-main.html',
   styleUrl: './login-main.scss',
 })
 export class LoginMain {
-
+  closeLoginForm =output();
+  formLogin=signal<FormGroup|null>(null);
+  constructor(private formBuilder: FormBuilder) {
+    this.formLogin.set(this.formBuilder.group({
+      "email":["",[Validators.email,Validators.minLength(5)]],
+      "password":["",[Validators.required,Validators.minLength(6)]],
+    }))
+  }
 }
